6e79d9f0054d00afed3f0f1b5afd786bded9802d
MaxBot <51864059+MaxBotCoder@users.noreply.github.com>
2024-08-19 17:20:47 -0700

    Add files via upload

---
+++ code.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+
+pragma soldity 0.8.14;
+
+contract NameOfMyContract {
+
+string public nameofstring = "Hello There";
+
+function (string memory _stringStorageLocation) public {
+
+
+	nameofstring = _stringStorageLocation;
+ 
+
+}
+
+
+}
